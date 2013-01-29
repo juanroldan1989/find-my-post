83592aa9b67512d1a95ec8da9b4750686420c422
Juan Roldán <juanroldan1989@gmail.com>
2013-01-29 20:31:26 -0300

    [Constants initializer]
     - SITE_URL fixed for dev and prod environment.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,9 @@
 APP_ID     = '250798808383711' # please change!
 APP_SECRET = '15df202b82dadc7071145d75d58dc6d5' # please change!
-SITE_URL   = 'http://localhost:3000'
-# SITE_URL   = 'http://organizeme.herokuapp.com'
+
+if Rails.env.production?
+  SITE_URL   = 'http://organizeme.herokuapp.com'
+else
+  SITE_URL   = 'http://localhost:3000'
+end
 
