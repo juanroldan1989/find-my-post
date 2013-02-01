1f25ab90921dcd48497403582a1435152c24ce18
Juan Roldán <juanroldan1989@gmail.com>
2013-02-01 15:01:37 -0300

    [Html tags and constants]
     - Http-equivalent tag removed from html. Layout application backed to original state.
     - SITE_URL backed to original (organizeme.herokuapp.com).
       Facebook app is responding to organizeme.herokuapp.com

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,7 +2,7 @@ APP_ID     = '250798808383711' # please change!
 APP_SECRET = '215bab18880861c6292cfedd12f54f56' # please change!
 
 if Rails.env.production?
-  SITE_URL   = 'http://encuentramipost.com.ar'
+  SITE_URL   = 'http://organizeme.herokuapp.com'
 else
   SITE_URL   = 'http://localhost:3000'
 end
