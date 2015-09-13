f811fdfb1df217a42de7fc2f2e379f27fbfc8c36
Juan Roldan <juanroldan1989@gmail.com>
2015-09-13 18:27:14 -0300

    HomeController - Indentation converted into spaces

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,55 +1,56 @@
 class HomeController < ApplicationController
-	layout :resolve_layout
+  layout :set_layout
 
   def index
-   	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/results')
-		@auth_url =  session[:oauth].url_for_oauth_code(:permissions => "user_groups") 	
-  	
-  	respond_to do |format|
-			 format.html { }
-		 end
+    session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/results')
+    @auth_url =  session[:oauth].url_for_oauth_code(:permissions => "user_groups")  
+    
+    respond_to do |format|
+       format.html { }
+     end
   end
 
   def results
-	 	if params[:code]
-		  session[:access_token] = session[:oauth].get_access_token(params[:code])
-		end		
+    if params[:code]
+      session[:access_token] = session[:oauth].get_access_token(params[:code])
+    end
 
-		# auth established, now do a graph call:
-		# @api = Koala::Facebook::API.new('AAACEdEose0cBAHtZC0NT37xfOfbP1keDQmeT46JyPFZC3lnDnDqZBRJfb8y41U12vSfH7hNT1ZBdYZBCO86jMDDf95FZCkZADKJtW8clSbbBgZDZD')
-		@api = Koala::Facebook::API.new(session[:access_token])
+    # auth established, now do a graph call:
+    # @api = Koala::Facebook::API.new('AAACEdEose0cBAHtZC0NT37xfOfbP1keDQmeT46JyPFZC3lnDnDqZBRJfb8y41U12vSfH7hNT1ZBdYZBCO86jMDDf95FZCkZADKJtW8clSbbBgZDZD')
+    @api = Koala::Facebook::API.new(session[:access_token])
 
-		begin
-			#consultas personales
-			@fbprofile  = @api.get_object("me")
-			@fbp_image  = @api.get_picture("me")
+    begin
+      #consultas personales
+      @fbprofile  = @api.get_object("me")
+      @fbp_image  = @api.get_picture("me")
 
-			fql          = "SELECT gid, name FROM group WHERE gid IN (SELECT gid FROM group_member WHERE uid = me()) ORDER BY name"
-			@user_groups = @api.fql_query(fql)
+      fql          = "SELECT gid, name FROM group WHERE gid IN (SELECT gid FROM group_member WHERE uid = me()) ORDER BY name"
+      @user_groups = @api.fql_query(fql)
 
-			#selecciono un grupo del dropdown
-			if params[:group]
-				#consultar grupo por su ID, no desde mi face: /me/groups/...
-				@graph_group_data    = @api.get_object("/" + params[:group], "fields" => "name,description,icon,feed.fields(from,message,name).limit(" + params[:posts_cant] + ")")
-			end
+      #selecciono un grupo del dropdown
+      if params[:group]
+        #consultar grupo por su ID, no desde mi face: /me/groups/...
+        @graph_group_data    = @api.get_object("/" + params[:group], "fields" => "name,description,icon,feed.fields(from,message,name).limit(" + params[:posts_cant] + ")")
+      end
 
-		rescue Exception=>ex
-			puts ex.message
-		end
+    rescue Exception=>ex
+      puts ex.message
+    end
   
- 		respond_to do |format|
-		 format.html {   }
-		end
+    respond_to do |format|
+     format.html { }
+    end
   end
 
-	private
-  def resolve_layout
-  	case action_name
-  	when "results"
-  		"results"
-  	else
-  		"application" 
-  	end
+  private
+
+  def set_layout
+    case action_name
+    when "results"
+      "results"
+    else
+      "application"
+    end
   end
 
 end
