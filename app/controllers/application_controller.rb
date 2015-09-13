d47f7ac89b6ef89b236af19ab408d37c73a64dae
Juan Roldan <juanroldan1989@gmail.com>
2015-09-13 19:51:10 -0300

    Application controller standarized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_filter :set_layout
+
 end
