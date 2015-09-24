6e80f438146ef36bbb766239bd4e9c5833ef38af
Juan Roldan <juanroldan1989@gmail.com>
2015-09-24 12:02:23 -0300

    Results helpers implemented for Groups and Posts dropdowns

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+
+  def groups_to_select_from
+    groups.collect {|s| [s["name"], s["gid"]]}
+  end
+
+  def posts_amount
+    [
+      ["20 posts", 20], ["40 posts", 40], ["60 posts", 60],
+      ["80 posts", 80], ["120 posts", 120], ["180 posts", 180],
+      ["250 posts", 250]
+    ]
+  end
 end
